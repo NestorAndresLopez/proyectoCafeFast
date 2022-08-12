b3b5cd6bb03b164fc53e9a92457f89f4cb24336f
NestorAndresLopez <n.andreslopez.nals@gmail.com>
2022-08-11 22:35:34 -0500

    proyectofast terminado, login signup crud proteger rutas

--- src/routes/authentications.js
+++ src/routes/authentications.js
@@ -2,20 +2,42 @@ const express = require('express');
 const router = express.Router();
 
 const passport = require('passport');
+const {isLoggedIn } = require('../lib/auth');
 
-router.get('/signup', (req, res)=>{
+//falta usar esta funcion para cuando ya este login no me muetre la ventana de registro y login 
+const {isNotLoggedIn} = require('../lib/auth');
+
+router.get('/signup',  isNotLoggedIn, (req, res)=>{
     res.render('auth/signup');
 })
 
-router.post('/signup', passport.authenticate('local.signup', {
-        successRedirect: '/profile',
+router.post('/signup',  isNotLoggedIn,passport.authenticate('local.signup', {
+        successRedirect: '/signin',
         failureRedirect: '/signup',
         failureFlash: true
     }))
 
+router.get('/signin',  (req, res)=>{
+    res.render('auth/signin');
+});
 
-router.get('/profile', (req, res)=>{
-    res.send('Todos los Perfiles')
+router.post('/signin', isNotLoggedIn, (req, res, next)=>{
+    passport.authenticate('local.signin',{
+        successRedirect: '/profile',
+        failureRedirect: '/signin',
+        failureFlash: true
+    })(req, res, next);
 })
+
+router.get('/profile', isLoggedIn, (req, res)=>{
+    res.render('profile')
+})
+
+router.get('/logout',  function(req, res, next) {
+    req.logout(function(err) {
+        if (err) { return next(err); }
+        res.redirect('/signin');
+    });
+});
 
 module.exports = router;
